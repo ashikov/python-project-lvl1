5c69c1ff44cd3c65f40591ef3e22be18c5e87972
Roman Ashikov <rmn.shkv@gmail.com>
2024-01-07 18:11:30 +0700

    implement engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,12 +1,26 @@
 from prompt import string
 
+
 def engine(game_data, description):
     print("Welcome to the Brain Games!")
     name = string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print(f"Hello, {name}!")
 
     print(description)
 
     for round_data in game_data:
-        question, answer = round_data.items()
-    print()
+        question = round_data['question']
+        answer = round_data['answer']
+
+        userAnswer = string(f"Question: {question}\n")
+
+        if userAnswer != answer:
+            f"{userAnswer} is wrong answer ;(. Correct answer was {answer}. Let\'s try again, {name}!" # noqa: E501
+
+            return
+
+        print('Correct!')
+
+    print(f"Congratulations, {name}!")
+
+    return
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,16 +1,17 @@
 from random import randint
-from engine import engine
-from constants import ROUNDS_COUNT, MIN, MAX
+from brain_games.engine import engine
+from brain_games.constants import ROUNDS_COUNT, MIN, MAX
 
-description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def is_even(number):
     return number % 2 == 0
 
+
 def generate_round_data(number):
-    answer = is_even(number) if "yes" else "no"
+    answer = "yes" if is_even(number) else "no"
 
     return {'question': number, 'answer': answer}
+
 
 def generate_game_data():
     data = []
@@ -18,8 +19,11 @@ def generate_game_data():
     while i <= ROUNDS_COUNT:
         number = randint(MIN, MAX)
         data.append(generate_round_data(number))
+        i += 1
     return data
 
-def run(data, description):
+
+def run():
+    description = 'Answer "yes" if the number is even, otherwise answer "no".'
     game_data = generate_game_data()
     engine(game_data, description)
