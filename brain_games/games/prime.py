995e0c3c0c933300b8642080d6674cf8cc345125
Roman Ashikov <rmn.shkv@gmail.com>
2024-01-07 23:46:22 +0700

    add prime game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,38 @@
+from random import randint
+from brain_games.engine import engine
+from brain_games.constants import ROUNDS_COUNT, MIN, MAX
+from math import sqrt
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    divisor = 3
+    while divisor <=sqrt(number):
+        if number % divisor == 0:
+            return False
+        divisor += 1
+    return True
+
+
+def generate_round_data(number):
+    answer = "yes" if is_prime(number) else "no"
+
+    return {'question': number, 'answer': answer}
+
+
+def generate_game_data():
+    data = []
+    i = 1
+    while i <= ROUNDS_COUNT:
+        number = randint(MIN, MAX)
+        data.append(generate_round_data(number))
+        i += 1
+    return data
+
+
+def run():
+    description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    game_data = generate_game_data()
+    engine(game_data, description)
